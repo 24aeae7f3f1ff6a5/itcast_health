9c1891b39c6ca6ec5871c767daadabd482373c3a
itcast_cyq <2865371455@qq.com>
2020-02-17 04:41:07 +0800

    七牛图片的工具类不知道啥原因自己又好了，之前是上传失败的

--- health_common/src/main/java/com/itheima/health/util/QiniuUtils.java
+++ health_common/src/main/java/com/itheima/health/util/QiniuUtils.java
@@ -45,7 +45,7 @@ public class QiniuUtils {
     /*字节上传*/
     //上传文件
     public static void upload2Qiniu(byte[] bytes, String fileName) {
-        /*//构造一个带指定Zone对象的配置类
+        //构造一个带指定Zone对象的配置类
         Configuration cfg = new Configuration(Zone.zone0());
         //...其他参数参考类注释
         UploadManager uploadManager = new UploadManager(cfg);
@@ -68,15 +68,14 @@ public class QiniuUtils {
             } catch (QiniuException ex2) {
                 //ignore
             }
-        }*/
+        }
 
-        //构造一个带指定 Region 对象的配置类
+        /*//构造一个带指定 Region 对象的配置类
         Configuration cfg = new Configuration(Zone.zone0());
-//...其他参数参考类注释
+        //...其他参数参考类注释
         UploadManager uploadManager = new UploadManager(cfg);
-//默认不指定key的情况下，以文件内容的hash值作为文件名
+        //默认不指定key的情况下，以文件内容的hash值作为文件名
         String key = fileName;
-        try {
             Auth auth = Auth.create(accessKey, secretKey);
             String upToken = auth.uploadToken(bucket);
 
@@ -94,10 +93,7 @@ public class QiniuUtils {
                 } catch (QiniuException ex2) {
                     //ignore
                 }
-            }
-        } catch (Exception ex) {
-            //ignore
-        }
+            }*/
     }
 
     //删除文件
